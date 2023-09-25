03091c8413a525bf917f9eb1e726da7aafa1bc55
Mario Papetti <mp.robots@gmail.com>
2023-09-24 23:20:48 -0300

    first commit

---
+++ cypress/integration/Ejercicio-1/ejercicio.spec.js
@@ -0,0 +1,10 @@
+
+context('Ejercicio N°1', () => {
+    beforeEach(() => {
+      cy.visit('https://example.cypress.io/commands/actions');
+    })
+
+    it('Caso 1', () => {
+        //asdasd
+    });
+});
